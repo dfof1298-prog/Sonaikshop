0283656980ee5041279b1fdb6a71b83926565fce
dfof1298-prog <dfof1298@gmail.com>
2026-06-19 11:44:27 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -49,7 +49,7 @@ JOIN_GROUP_ID = -1002635018188
 JOIN_CHANNEL_ID = -1002635018188
 JOIN_GROUP_LINK = "https://t.me/ReGict7"
 JOIN_CHANNEL_LINK = "https://t.me/ReGict7"
-API_BASE_URL = "http://62.72.20.10:8081/shopify"
+API_BASE_URL = "https://deadline-shopify-production-2b43.up.railway.app/shopify"
 
 # Payment bot username
 PAYMENT_BOT_USERNAME = "Stars838bot"
