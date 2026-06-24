3bb98c7480be1dee8f474761e58ab65dda4b28fe
dfof1298-prog <dfof1298@gmail.com>
2026-06-25 02:01:51 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -120,7 +120,7 @@ JOIN_GROUP_LINK = "https://t.me/ReGict7"
 
 JOIN_CHANNEL_LINK = "https://t.me/ReGict7"
 
-API_BASE_URL = "https://shopan-production.up.railway.app/shopify"
+API_BASE_URL = "https://web-production-3d364.up.railway.app/shopify"
 
 
 
